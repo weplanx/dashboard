50b7de6a876252856192e41d02bc611924e19908
kain <zhangtqx@vip.qq.com>
2018-09-14 09:48:12 +0800

    Modify:Lib

--- projects/ngx-bit/src/public_api.ts
+++ projects/ngx-bit/src/public_api.ts
@@ -3,7 +3,6 @@
  */
 
 export * from './lib/ngx-bit.module';
-export * from './lib/ngx-bit.pipe.module';
 
 export * from './lib/plugins/i18n-switch/i18n-switch.module';
 
@@ -19,6 +18,9 @@ export * from './lib/common/lists.service';
 export * from './lib/common/origin-lists.service';
 export * from './lib/common/status.service';
 export * from './lib/common/swal.service';
+
+export * from './lib/pipe/json-parse.pipe';
+export * from './lib/pipe/empty-object.pipe';
 
 export * from './lib/operates';
 
