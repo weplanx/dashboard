177516dd407152e0c87d7f7568f217d4ab082ba8
kain <zhangtqx@vip.qq.com>
2018-09-14 10:23:42 +0800

    Modify:Lib

--- projects/ngx-bit/src/lib/ngx-bit.module.ts
+++ projects/ngx-bit/src/lib/ngx-bit.module.ts
@@ -1,8 +1,7 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {LocalStorage} from '@ngx-pwa/local-storage';
 
-import {NgxBitModuleOptions} from './interface';
 import {ConfigService} from './config.service';
 import {BitService} from './bit.service';
 import {EventsService} from './events.service';
@@ -17,75 +16,32 @@ import {DeleteService} from './common/delete.service';
 import {StatusService} from './common/status.service';
 import {SwalService} from './common/swal.service';
 
-import {} from './pipe/empty-object.pipe';
-import {} from './pipe/json-parse.pipe';
+import {EmptyObjectPipe} from './pipe/empty-object.pipe';
+import {JsonParsePipe} from './pipe/json-parse.pipe';
 
 @NgModule({
   imports: [
     HttpClientModule
   ],
-  declarations: []
+  declarations: [
+    EmptyObjectPipe,
+    JsonParsePipe
+  ],
+  providers: [
+    ConfigService,
+    LocalStorage,
+    BitService,
+    HttpService,
+    EventsService,
+    AddService,
+    DeleteService,
+    EditService,
+    GetService,
+    ListsService,
+    OriginListsService,
+    StatusService,
+    SwalService
+  ]
 })
 export class NgxBitModule {
-  static forRoot(options: NgxBitModuleOptions): ModuleWithProviders {
-    if (!options.language) {
-      options.language = {};
-    }
-    if (!options.withCredentials) {
-      options.withCredentials = true;
-    }
-    if (!options.i18n) {
-      options.i18n = ['zh_cn', 'en_us'];
-    }
-    if (!options.i18n_switch) {
-      options.i18n_switch = [
-        {
-          i18n: 'zh_cn',
-          name: {
-            zh_cn: '中文',
-            en_us: 'Chinese'
-          }
-        },
-        {
-          i18n: 'en_us',
-          name: {
-            zh_cn: '英文',
-            en_us: 'English'
-          }
-        }
-      ];
-    }
-    if (!options.page_limit) {
-      options.page_limit = 20;
-    }
-
-    return {
-      ngModule: NgxBitModule,
-      providers: [
-        {
-          provide: ConfigService,
-          useValue: {
-            origin: options.origin,
-            language: options.language,
-            withCredentials: options.withCredentials,
-            i18n: options.i18n,
-            i18n_switch: options.i18n_switch,
-            page_limit: options.page_limit
-          }
-        },
-        LocalStorage,
-        BitService,
-        HttpService,
-        EventsService,
-        AddService,
-        DeleteService,
-        EditService,
-        GetService,
-        ListsService,
-        OriginListsService,
-        StatusService,
-        SwalService
-      ]
-    };
-  }
 }
