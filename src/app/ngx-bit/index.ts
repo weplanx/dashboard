9285fb764a8bde4462c472ef20430660018dd667
kain <zhangtqx@vip.qq.com>
2018-10-19 18:02:37 +0800

    Remove Old Develop

--- src/app/ngx-bit/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/bit.service';
-export * from './lib/events.service';
-export * from './lib/http.service';
-export * from './lib/ngx-bit.module';
--- src/app/ngx-bit/pipe/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './json-parse.pipe';
-export * from './empty-object.pipe';
