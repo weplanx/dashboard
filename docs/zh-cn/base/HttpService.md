017d1aa0df85805f664915b145bfb509a55a1039
kain <zhangtqx@vip.qq.com>
2018-10-23 17:13:50 +0800

    废除：前端存储Token与前端自动刷新超时Token

--- docs/zh-cn/base/HttpService.md
+++ docs/zh-cn/base/HttpService.md
@@ -1,26 +1,15 @@
 # 请求服务 - HttpService
 
-> `HttpService` 是对 `Angular HttpClient` 的增强封装，在调用 `req` 函数时不仅是对配置信息组合，在请求的过程中还做了登录校验、令牌校验以及自动刷新令牌等。
+> `HttpService` 是对配置组合的请求封装服务。
 
-##### `req(url: string, body: any = {}, jwt = true): Observable<any>`
+##### `req(url: string, body: any = {}): Observable<any>`
 
 - 请求对象合成，默认为 `post` 请求
 - `url` RESTful请求路由地址
 - `body` 发送数据
-- `jwt` 是否需要jwt验证
 
-例子1：生成不验证jwt的请求对象
 
-```typescript
-this.http.req('main/login', {
-    username: 'nobody',
-    password: '123'
-}, false).subscribe(res => {
-    console.log(res);
-});
-```
-
-例子2：生成验证jwt的请求对象
+生成验证jwt的请求对象
 
 ```typescript
 this.http.req('main/nav').subscribe(res => {
--- projects/ngx-bit/src/lib/http.service.ts
+++ projects/ngx-bit/src/lib/http.service.ts
@@ -7,106 +7,13 @@ import {ConfigService} from './config.service';
 
 @Injectable()
 export class HttpService {
-  private token: any;
-  private refresh_token: any;
-
   constructor(private http: HttpClient,
-              private config: ConfigService,
-              private storage: LocalStorage) {
+              private config: ConfigService) {
   }
 
-  private isLogin(): Observable<boolean> {
-    return this.storage.getItem('username').pipe(
-      switchMap(data => {
-        if (data) {
-          return this.storage.getItem('token');
-        } else {
-          return of(false);
-        }
-      }),
-      switchMap(data => {
-        if (data) {
-          this.token = data;
-          return this.storage.getItem('refresh_token');
-        } else {
-          return of(false);
-        }
-      }),
-      switchMap(data => {
-        if (data) {
-          this.refresh_token = data;
-          return of(true);
-        } else {
-          return of(false);
-        }
-      })
-    );
-  }
-
-  req(url: string, body: any = {}, jwt = true): Observable<any> {
-    if (!jwt) {
-      return this.client(url, body);
-    } else {
-      return this.isLogin().pipe(
-        switchMap(status => {
-          if (status) {
-            return this.clientWithToken(url, body);
-          } else {
-            return of({
-              error: 1
-            });
-          }
-        })
-      );
-    }
-  }
-
-  private client(url: string, body: any = {}): Observable<any> {
+  req(url: string, body: any = {}): Observable<any> {
     return this.http.post(this.config.origin + this.config.namespace + '/' + url, body, {
       withCredentials: this.config.withCredentials
     });
-  }
-
-  private clientWithToken(url: string, body: any = {}): Observable<any> {
-    return this.http.post(this.config.origin + this.config.namespace + '/' + url, body, {
-      headers: new HttpHeaders({
-        'X-Token': this.token
-      }),
-      withCredentials: this.config.withCredentials
-    }).pipe(
-      switchMap((res: any) => {
-        if (res.error === 1 &&
-          res.msg !== undefined &&
-          res.msg === 'Expired token') {
-          return this.requestTokenExpired(url, body);
-        } else {
-          return of(res);
-        }
-      })
-    );
-  }
-
-  private requestTokenExpired(url: string, data: any = {}): Observable<any> {
-    return this.req('main/refreshToken', {
-      refresh_token: this.refresh_token
-    }, false).pipe(
-      switchMap(res => {
-        if (!res.error) {
-          this.token = res.data.token;
-          return this.storage.setItem('token', res.data.token);
-        } else {
-          return of(false);
-        }
-      }),
-      switchMap(status => {
-        if (status) {
-          return this.clientWithToken(url, data);
-        } else {
-          return of({
-            error: 1
-          });
-        }
-      })
-    );
   }
 }
