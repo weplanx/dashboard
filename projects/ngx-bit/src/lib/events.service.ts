85551512a11eb66fde2d85afda80a84d3ec07ac7
kain <zhangtqx@vip.qq.com>
2018-10-31 15:39:35 +0800

    事件通讯调整

--- projects/ngx-bit/src/lib/events.service.ts
+++ projects/ngx-bit/src/lib/events.service.ts
@@ -1,13 +1,13 @@
-import {Injectable} from '@angular/core';
-import {Observable, Subject} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable()
 export class EventsService {
   private events: Map<string, Subject<any>> = new Map<string, Subject<any>>();
 
   publish(topic: string, args?: any) {
-    const topics = this.events.get(topic);
-    if (topics) {
+    if (this.events.has(topic)) {
+      const topics = this.events.get(topic);
       topics.next((args !== undefined) ? args : null);
     } else {
       this.events.set(topic, new Subject()).get(topic).next(args);
@@ -15,8 +15,8 @@ export class EventsService {
   }
 
   on(topic: string): Observable<any> {
-    const topics = this.events.get(topic);
-    if (topics) {
+    if (this.events.has(topic)) {
+      const topics = this.events.get(topic);
       return topics;
     } else {
       return this.events.set(topic, new Subject()).get(topic);
@@ -24,8 +24,8 @@ export class EventsService {
   }
 
   off(topic: string) {
-    const topics = this.events.get(topic);
-    if (topics) {
+    if (this.events.has(topic)) {
+      const topics = this.events.get(topic);
       topics.unsubscribe();
       this.events.delete(topic);
     }
