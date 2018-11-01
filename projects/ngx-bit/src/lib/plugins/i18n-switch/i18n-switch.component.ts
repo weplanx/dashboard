6c4a9a4541e54c22e405dc78cac49751839d6be1
kain <zhangtqx@vip.qq.com>
2018-11-01 15:56:54 +0800

    Update:i18n-switch

--- projects/ngx-bit/src/lib/plugins/i18n-switch/i18n-switch.component.ts
+++ projects/ngx-bit/src/lib/plugins/i18n-switch/i18n-switch.component.ts
@@ -7,9 +7,20 @@ import {ConfigService} from '../../config.service';
   templateUrl: './i18n-switch.component.html'
 })
 export class I18nSwitchComponent {
-  @Input() i18n: string;
-  @Output() change: EventEmitter<any> = new EventEmitter();
+  _i18n = 'zh_cn';
   switch: any[] = [];
+
+  @Output() change: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  get i18n() {
+    return this._i18n;
+  }
+
+  set i18n(_i18n: string) {
+    this._i18n = _i18n;
+    this.change.emit(this._i18n);
+  }
 
   constructor(public bit: BitService,
               config: ConfigService) {
