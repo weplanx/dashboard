29ddd631e2adf49a1eb916f21dc18f679ffdd0b8
kain <zhangtqx@vip.qq.com>
2018-11-05 17:18:33 +0800

    Update:Add Config Options

--- projects/ngx-bit/src/lib/config.service.ts
+++ projects/ngx-bit/src/lib/config.service.ts
@@ -2,12 +2,18 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class ConfigService {
-  origin: string;
-  namespace: string;
-  static: string;
-  uploads: string;
-  withCredentials: boolean;
-  i18n: any[];
-  i18n_switch: any[];
-  page_limit: number;
+  origin = '';
+  namespace = '';
+  static = '';
+  uploads = '';
+  with_credentials = false;
+  http_customize = false;
+  i18n: any[] = ['zh_cn'];
+  i18n_switch: any[] = [{
+    i18n: 'zh_cn', name: {
+      zh_cn: '中文',
+      en_us: 'Chinese'
+    }
+  }];
+  page_limit = 20;
 }
--- projects/ngx-bit/src/lib/http.service.ts
+++ projects/ngx-bit/src/lib/http.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {ConfigService} from './config.service';
+import {switchMap} from 'rxjs/operators';
 
 @Injectable()
 export class HttpService {
@@ -10,8 +11,19 @@ export class HttpService {
   }
 
   req(url: string, body: any = {}): Observable<any> {
-    return this.http.post(this.config.origin + this.config.namespace + '/' + url, body, {
-      withCredentials: this.config.withCredentials
+    const http = this.http.post(this.config.origin + this.config.namespace + '/' + url, body, {
+      withCredentials: this.config.with_credentials
     });
+    if (!this.config.http_customize) {
+      return http;
+    } else {
+      return http.pipe(
+        switchMap(res => this.customize(res))
+      );
+    }
+  }
+
+  customize(res: any): Observable<any> {
+    return of(res);
   }
 }
