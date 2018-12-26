d0825c06221bba5db51fa42f039d6fac06ca7a1b
kain <zhangtqx@vip.qq.com>
2018-12-26 17:53:07 +0800

    update: status.service

--- projects/ngx-bit/src/lib/common/status.service.ts
+++ projects/ngx-bit/src/lib/common/status.service.ts
@@ -1,32 +1,37 @@
-import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
-import { HttpService } from '../http.service';
+import {Injectable} from '@angular/core';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {HttpService} from '../http.service';
 
 @Injectable()
 export class StatusService {
-	private action = '/edit';
+  private action = '/edit';
 
-	constructor(private http: HttpService) {}
+  constructor(private http: HttpService) {
+  }
 
-	customAction(name: string) {
-		this.action = name;
-	}
+  customAction(name: string) {
+    this.action = name;
+  }
 
-	factory(model: string, data: any, field = 'status'): Observable<any> {
-		return this.http
-			.req(model + this.action, {
-				id: data.id,
-				switch: true,
-				[field]: !data[field]
-			})
-			.pipe(
-				map((res) => {
-					if (!res.error) {
-						data[field] = !data[field];
-					}
-					return res;
-				})
-			);
-	}
+  factory(model: string, data: any, field = 'status', extra?: any): Observable<any> {
+    let body = {
+      id: data.id,
+      switch: true,
+      [field]: !data[field]
+    };
+
+    if (extra) {
+      body = Object.assign(body, extra);
+    }
+
+    return this.http.req(model + this.action, body).pipe(
+      map((res) => {
+        if (!res.error) {
+          data[field] = !data[field];
+        }
+        return res;
+      })
+    );
+  }
 }
