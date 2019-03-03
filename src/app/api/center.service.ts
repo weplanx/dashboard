84f632321e88cfbc74e34805c05227dec121f476
kain <zhangtqx@vip.qq.com>
2019-03-03 14:11:45 +0800

    Update:projects

--- src/app/api/center.service.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable} from '@angular/core';
-import {Observable, of} from 'rxjs';
-
-@Injectable()
-export class CenterService {
-  clear(): Observable<boolean> {
-    return of(true);
-  }
-}
