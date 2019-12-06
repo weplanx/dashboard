399a14ee45a88810cc315df66452a619f8d8ddca
kain <zhangtqx@vip.qq.com>
2019-12-06 11:45:07 +0800

    Update Split

--- src/lib/pipe/split.pipe.ts
+++ src/lib/pipe/split.pipe.ts
@@ -3,6 +3,9 @@ import {Pipe, PipeTransform} from '@angular/core';
 @Pipe({name: 'Split'})
 export class SplitPipe implements PipeTransform {
   transform(text: string, symbol: string): string[] {
+    if (!text || !symbol) {
+      return [];
+    }
     return text.split(symbol);
   }
 }
