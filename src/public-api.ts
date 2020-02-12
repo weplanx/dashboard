d1c495d4c0f7ef49371610eae8fca6f792ae548c
kain <zhangtqx@vip.qq.com>
2020-02-12 11:48:52 +0800

    Remove Mgo Service

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,7 +2,6 @@ export * from './lib/common/config.service';
 export * from './lib/common/bit.service';
 export * from './lib/common/events.service';
 export * from './lib/common/http.service';
-export * from './lib/common/mgo.service';
 
 export * from './lib/plugin/swal.service';
 export * from './lib/plugin/storage.service';
@@ -12,4 +11,4 @@ export * from './lib/ngx-bit.pipe.module';
 export * from './lib/ngx-bit.directive.module';
 export * from './lib/ngx-bit.ext.module';
 
-export *  from './lib/lib.operates';
+export * from './lib/lib.operates';
