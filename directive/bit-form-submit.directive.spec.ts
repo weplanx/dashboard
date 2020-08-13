a8f676f654553e51e904e877985e99cf9124db8a
kain <zhangtqx@vip.qq.com>
2020-08-13 11:27:53 +0800

    更新单元测试

---
+++ directive/bit-form-submit.directive.spec.ts
@@ -0,0 +1,85 @@
+/* tslint:disable:component-class-suffix */
+import { Component, DebugElement, OnInit } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { BitConfigService, NgxBitModule } from 'ngx-bit';
+import { BitDirectiveModule } from 'ngx-bit/directive';
+import { environment } from '../simulation/environment';
+
+describe('BitFormSubmitDirective', () => {
+  let config: BitConfigService;
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let debugElement: DebugElement;
+
+  beforeEach(() => {
+    if (!component) {
+      TestBed.configureTestingModule({
+        declarations: [
+          TestComponent
+        ],
+        imports: [
+          FormsModule,
+          ReactiveFormsModule,
+          BitDirectiveModule,
+          RouterModule.forRoot([]),
+          NgxBitModule.forRoot(environment.bit)
+        ]
+      });
+      config = TestBed.inject(BitConfigService);
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      debugElement = fixture.debugElement;
+      fixture.detectChanges();
+    }
+  });
+
+  it('Test form submit automatically markAsDirty and updateValueAndValidity', () => {
+    const form = debugElement.query(By.css('form'));
+    form.triggerEventHandler('submit', null);
+    expect(component.form.valid).toBeFalsy();
+    component.form.patchValue({
+      name: 'kain',
+      email: 'zhangtqx@vip.qq.com'
+    });
+    form.triggerEventHandler('submit', null);
+    expect(component.form.valid).toBeTruthy();
+    expect(component.resultValue).toEqual({
+      name: 'kain',
+      email: 'zhangtqx@vip.qq.com',
+      status: 1
+    });
+  });
+});
+
+@Component({
+  template: `
+    <form [formGroup]="form" (bitFormSubmit)="submit($event)">
+      <input formControlName="name"/>
+      <input formControlName="email"/>
+    </form>
+  `
+})
+class TestComponent implements OnInit {
+  form: FormGroup;
+  resultValue: any;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      name: [null, [Validators.required]],
+      email: [],
+      status: [1, [Validators.required]]
+    });
+  }
+
+  submit(data) {
+    this.resultValue = data;
+  }
+}
