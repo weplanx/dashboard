c47446e51055f9f6ff6585b53826b42cd9d5c589
kain <zhangtqx@vip.qq.com>
2020-08-16 00:48:37 +0800

    update travis

--- projects/ngx-bit/karma.conf.js
+++ projects/ngx-bit/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ngx-bit'),
+      dir: require('path').join(__dirname, '../../coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
