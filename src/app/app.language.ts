f486e22286e1f2d39615e6d0b2205823fee48e0d
kain <zhangtqx@vip.qq.com>
2021-01-29 17:53:01 +0800

    update

--- src/app/app.language.ts
+++ src/app/app.language.ts
@@ -1,7 +1,7 @@
 export default {
   // The Dashboard Language Pack
   dashboard: ['仪表盘', 'Dashboard'],
-  language: ['中文', 'English'],
+  language: ['切换语言', 'Language'],
   center: ['个人中心', 'Center'],
   profile: ['信息修改', 'Profile'],
   exit: ['退出系统', 'Exit'],
