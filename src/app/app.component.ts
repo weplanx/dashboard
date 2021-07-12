5f62bda78488c04f7f7a265e0ddbef7654bc9e40
kain <zhangtqx@vip.qq.com>
2021-07-13 01:04:55 +0800

    update v12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BitHttpService, BitService } from 'ngx-bit';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  template: ` <router-outlet></router-outlet> `
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(
+    private bit: BitService,
+    private http: BitHttpService,
+    private notification: NzNotificationService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.bit.setupLocale();
+    this.bit.registerLocales(import('./app.language'));
+    this.http.setupInterceptor(
+      map(res => {
+        if (res.error) {
+          switch (res.msg) {
+            case 'refresh token verification expired':
+              this.notification.warning(this.bit.l.auth, this.bit.l.authInvalid, {
+                nzKey: 'authInvalid'
+              });
+              this.router.navigateByUrl('/login');
+              break;
+          }
+        }
+        return res;
+      })
+    );
+  }
+}
---
+++ src/app/app.language.ts
@@ -0,0 +1,106 @@
+export default {
+  // 登录页面语言包
+  welcome: ['欢迎访问企业测试系统', 'Welcome to Your Compony'],
+  subTitle: ['遵循 Ant Design 设计规范的 Angular 企业系统', 'An Angular enterprise system based on Ant Design'],
+  footer: [
+    '基于 ng-zorro-antd 强力驱动，由 kain 整合开发的系统解决方案',
+    'Based on the drive of ng-zorro-antd, a system solution integrated and developed by kain'
+  ],
+
+  // 仪表盘语言包
+  dashboard: ['仪表盘', 'Dashboard'],
+  language: ['切换语言', 'Language'],
+  center: ['个人中心', 'Center'],
+  profile: ['信息修改', 'Profile'],
+  exit: ['退出系统', 'Exit'],
+
+  // 公共语言包
+  add: ['新增', 'Add'],
+  edit: ['编辑', 'Edit'],
+  delete: ['删除', 'Delete'],
+  submit: ['提交', 'Submit'],
+  reset: ['重置', 'Reset'],
+  cancel: ['取消', 'Cancel'],
+  back: ['返回', 'Back'],
+  status: ['状态', 'Status'],
+  on: ['开启', 'On'],
+  off: ['冻结', 'Off'],
+  yes: ['是', 'Yes'],
+  no: ['否', 'No'],
+  action: ['操作', 'Action'],
+  upload: ['上传', 'Upload'],
+  refreshLists: ['刷新列表', 'Refresh Lists'],
+  clearSearch: ['清除搜索', 'Clear Search'],
+  bulkDelete: ['批量删除', 'Bulk Delete'],
+  startTime: ['开始日期', 'Start Time'],
+  endTime: ['结束日期', 'End Time'],
+
+  // The Notification or Message Language Pack
+  success: ['操作成功', 'Operation Success'],
+  error: ['操作失败', 'Operation Failed'],
+  updateSuccess: [
+    '您的请求提交成功，已更新数据内容',
+    'Your request was submitted successfully and the data content has been updated'
+  ],
+  updateError: ['您的请求提交异常，请稍后再试', 'Your request submission is abnormal, please try again later'],
+  deleteSuccess: [
+    '您的请求提交成功，该数据已被删除',
+    'Your request was submitted successfully and the data has been deleted'
+  ],
+  deleteError: ['您的请求提交异常，请稍后再试', 'Your request submission is abnormal, please try again later'],
+  uploadSuccess: ['您的文件已上传完毕', 'Your file has been uploaded'],
+  uploadError: ['您的文件未能上传，请稍后再试', 'Your file failed to upload, please try again later'],
+  auth: ['认证提示', 'Authorization Information'],
+  authLogout: ['您已退出管理系统', 'You have logged out of the management system'],
+  authInvalid: ['您的登录认证失效，请重新登录', 'Your login authentication is invalid, please log in again'],
+  roleError: [
+    '您没有权限加载此模块或当前模块已被屏蔽',
+    `You do not have permission to load this module or the current module has been blocked`
+  ],
+
+  // The Tooltip Language Pack
+  I18nNoTip: ['无提示', 'No prompt'],
+  I18nZHCN: ['中文为空', 'Chinese empty'],
+  I18nENUS: ['英文为空', 'English empty'],
+  i18nTip: ['多语言输入框', 'A multilingual input box'],
+
+  // The Async Validate
+  validating: ['正在验证...', 'Validating...'],
+
+  // The Status Language Pack
+  StatusSuccess: [
+    '您的请求提交成功，已更新数据状态',
+    'Your request was submitted successfully and the data status has been updated'
+  ],
+  StatusError: ['您的请求提交异常，请稍后再试', 'Your request submission is abnormal, please try again later'],
+
+  // The Alert Language Pack
+  AddAlertSuccessTitle: ['操作成功', 'Operation Success'],
+  AddAlertSuccessContent: [
+    '表单数据已提交完成，您是否要继续执行新增操作？',
+    'The form data has been submitted. Do you want to continue with the new operation?'
+  ],
+  AddAlertSuccessOk: ['继续新增', 'Continue'],
+  AddAlertSuccessCancel: ['返回列表', 'Back to list'],
+  AddAlertErrorTitle: ['操作失败', 'Operation Failed'],
+  AddAlertErrorContent: ['表单数据提交异常，请稍后再试', 'Form data submission exception, please try again later'],
+  AddAlertErrorOk: ['好的', 'Ok'],
+  EditAlertSuccessTitle: ['操作成功', 'Operation Success'],
+  EditAlertSuccessContent: [
+    '表单数据已提交完成，你是否要继续执行编辑操作？',
+    'The form data has been submitted. Do you want to continue with the new operation?'
+  ],
+  EditAlertSuccessOk: ['继续编辑', 'Continue'],
+  EditAlertSuccessCancel: ['返回列表', 'Back to list'],
+  EditAlertErrorTitle: ['操作失败', 'Operation Failed'],
+  EditAlertErrorContent: ['表单数据提交异常，请稍后再试', 'Form data submission exception, please try again later'],
+  EditAlertErrorOk: ['好的', 'Ok'],
+  DeleteAlertTitle: ['请求确认', 'Operation Question'],
+  DeleteAlertContent: ['您确认删除当前数据吗？', 'Are you sure to delete the current data?'],
+  DeleteAlertOk: ['确认删除', 'Confirm'],
+  DeleteAlertCancel: ['再想想', 'Think again'],
+
+  // Transport
+  Transport: ['传输详情', 'Transport'],
+  Uploading: ['请勿切换页面，目前正在上传文件中', 'Do not switch pages, currently uploading files']
+};
