7441932fcaa9e52ed7cea1479391304bb4c8f9e8
kain <zhangtqx@vip.qq.com>
2021-07-17 09:05:51 +0800

    simplify

--- projects/ngx-bit/bit-config.ts
+++ projects/ngx-bit/bit-config.ts
@@ -21,25 +21,14 @@ export class BitConfig {
    */
   grid: Grid = {
     label: {
-      nzXXl: 4,
-      nzXl: 5,
-      nzLg: 6,
-      nzMd: 7,
-      nzSm: 24
+      nzSpan: 7
     },
     control: {
-      nzXXl: 8,
-      nzXl: 9,
-      nzLg: 10,
-      nzMd: 14,
-      nzSm: 24
+      nzSpan: 12
     },
     submit: {
-      nzXXl: { span: 8, offset: 4 },
-      nzXl: { span: 9, offset: 5 },
-      nzLg: { span: 10, offset: 6 },
-      nzMd: { span: 14, offset: 6 },
-      nzSm: { span: 24, offset: 0 }
+      nzOffset: 7,
+      nzSpan: 12
     }
   };
   /**
