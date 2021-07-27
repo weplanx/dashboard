9f2ff8481e7f968d6af83282b321c93e2235e4c0
kain <zhangtqx@vip.qq.com>
2021-07-27 21:59:46 +0800

    项目重构

--- src/app/pages/center/basic/basic.component.ts
+++ src/app/pages/center/basic/basic.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-center-basic',
   templateUrl: './basic.component.html'
 })
-export class BasicComponent {}
+export class BasicComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({});
+  }
+}
