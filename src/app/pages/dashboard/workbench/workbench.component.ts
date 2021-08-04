f0ec5b2d008ddd94e72ac903ab371f3484d2083c
kain <zhangtqx@vip.qq.com>
2021-08-04 16:39:22 +0800

    项目重构

--- src/app/pages/dashboard/workbench/workbench.component.ts
+++ src/app/pages/dashboard/workbench/workbench.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { AppService } from '@common/app.service';
 
@@ -11,42 +11,42 @@ export class WorkbenchComponent {
   projects: any[] = [
     {
       title: 'Alipay',
-      avatar: '/assets/workbench/alipay.png',
+      avatar: '/assets/logo/alipay.png',
       description: '那是一种内在的东西，他们到达不了，也无法触及的',
       group: '科学搬砖组',
       time: '几秒前'
     },
     {
       title: 'Angular',
-      avatar: '/assets/workbench/angular.png',
+      avatar: '/assets/logo/angular.png',
       description: '希望是一个好东西，也许是最好的，好东西是不会消亡的',
       group: '全组都是吴彦祖',
       time: '4 年前'
     },
     {
       title: 'Ant Design',
-      avatar: '/assets/workbench/antd.png',
+      avatar: '/assets/logo/antd.png',
       description: '城镇中有那么多的酒馆，她却偏偏走进了我的酒馆',
       group: '中二少女团',
       time: '几秒前'
     },
     {
       title: 'Ant Design Pro',
-      avatar: '/assets/workbench/antd-pro.png',
+      avatar: '/assets/logo/antd-pro.png',
       description: '那时候我只会想自己想要什么，从不想自己拥有什么',
       group: '程序员日常',
       time: '4 年前'
     },
     {
       title: 'Bootstrap',
-      avatar: '/assets/workbench/bootstrap.png',
+      avatar: '/assets/logo/bootstrap.png',
       description: '凛冬将至',
       group: '高逼格设计天团',
       time: '4 年前'
     },
     {
       title: 'React',
-      avatar: '/assets/workbench/react.png',
+      avatar: '/assets/logo/react.png',
       description: '生命就像一盒巧克力，结果往往出人意料',
       group: '骗你来学计算机',
       time: '4 年前'
--- src/app/pages/list/card/card.component.ts
+++ src/app/pages/list/card/card.component.ts
@@ -2,6 +2,44 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list-card',
-  templateUrl: './card.component.html'
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
 })
-export class CardComponent {}
+export class CardComponent {
+  data: any[] = [
+    {
+      title: 'Alipay',
+      avatar: '/assets/logo/alipay.png'
+    },
+    {
+      title: 'Angular',
+      avatar: '/assets/logo/angular.png'
+    },
+    {
+      title: 'Ant Design',
+      avatar: '/assets/logo/antd.png'
+    },
+    {
+      title: 'Ant Design Pro',
+      avatar: '/assets/logo/antd-pro.png'
+    },
+    {
+      title: 'Bootstrap',
+      avatar: '/assets/logo/bootstrap.png'
+    },
+    {
+      title: 'React',
+      avatar: '/assets/logo/react.png'
+    },
+    {
+      title: 'Vue',
+      avatar: '/assets/logo/vue.png'
+    },
+    {
+      title: 'Webpack',
+      avatar: '/assets/logo/webpack.png'
+    }
+  ];
+  content =
+    '在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。';
+}
