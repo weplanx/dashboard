19f7f803ac17c145e73df4aa96712c9cdc223a22
kain <zhangtqx@vip.qq.com>
2021-10-26 17:26:40 +0800

    update

--- projects/ngx/lowcode/wpx-template/wpx-template-table/wpx-template-table.component.ts
+++ projects/ngx/lowcode/wpx-template/wpx-template-table/wpx-template-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { WpxListByPage } from '@weplanx/ngx';
 import { NzCheckBoxOptionInterface } from 'ng-zorro-antd/checkbox';
@@ -15,15 +16,30 @@ export class WpxTemplateTableComponent implements OnChanges {
   @Input() fields!: Field[];
   @Input() lists!: WpxListByPage;
   @Output() readonly refresh: EventEmitter<void> = new EventEmitter<void>();
+  private fieldsMap: Map<string, Field> = new Map<string, Field>();
+
+  searchOptions: NzCheckBoxOptionInterface[] = [];
+
+  search: Field[] = [];
+  searchForm!: FormGroup;
 
   size: NzTableSize = 'middle';
+
   columnsChecked = true;
   columnsIndeterminate = false;
   columns: NzCheckBoxOptionInterface[] = [];
   displayColumns: NzCheckBoxOptionInterface[] = [];
 
+  constructor(private fb: FormBuilder) {}
+
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.hasOwnProperty('fields')) {
+      this.fieldsMap = new Map<string, Field>((changes.fields.currentValue as Field[]).map(v => [v.key, v]));
+      this.searchOptions = [
+        ...(changes.fields.currentValue as Field[])
+          .filter(v => !['media'].includes(v.type))
+          .map(v => <NzCheckBoxOptionInterface>{ label: v.label, value: v.key })
+      ];
       this.columns = [
         ...(changes.fields.currentValue as Field[]).map(
           v => <NzCheckBoxOptionInterface>{ label: v.label, value: v.key, checked: true }
@@ -45,5 +61,21 @@ export class WpxTemplateTableComponent implements OnChanges {
     this.columnsChecked = this.columns.every(v => v.checked);
     this.columnsIndeterminate = !this.columnsChecked && this.columns.some(v => v.checked);
     this.displayColumns = [...this.columns.filter(v => v.checked)];
+  }
+
+  updateSearch(): void {
+    const controls: any = {};
+    this.search = this.searchOptions
+      .filter(v => v.checked)
+      .map(v => {
+        const field = this.fieldsMap.get(v.value)!;
+        controls[field.key] = [];
+        return field;
+      });
+    this.searchForm = this.fb.group(controls);
+  }
+
+  submit(data: any): void {
+    console.log(data);
   }
 }
