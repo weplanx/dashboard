c84d057877a7daa5236bed5d9fc9fad48c63b630
kain <zhangtqx@vip.qq.com>
2021-12-18 07:06:02 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppShareModule } from '@share';
-import { WpxActivated } from '@weplanx/components';
-import { WpxSettingsModule } from '@weplanx/components/settings';
 
-import { WpxTemplateComponent } from '../../projects/components/template/wpx-template.component';
-import { WpxTemplateModule } from '../../projects/components/template/wpx-template.module';
 import { LayoutComponent } from './layout/layout.component';
 import { LayoutModule } from './layout/layout.module';
 
@@ -14,7 +10,6 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    // canActivateChild: [WpxActivated],
     children: [
       {
         path: 'dashboard',
@@ -23,11 +18,11 @@ const routes: Routes = [
       {
         path: 'center',
         loadChildren: () => import('./center/center.module').then(m => m.CenterModule)
-      },
-      {
-        path: ':fragments',
-        component: WpxTemplateComponent
       }
+      // {
+      //   path: ':fragments',
+      //   component: WpxTemplateComponent
+      // }
       // { path: '', redirectTo: '/app/dashboard', pathMatch: 'full' },
       // { path: '**', loadChildren: () => import('./empty/empty.module').then(m => m.EmptyModule) }
     ]
@@ -35,6 +30,6 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [AppShareModule, LayoutModule, WpxSettingsModule, WpxTemplateModule, RouterModule.forChild(routes)]
+  imports: [AppShareModule, LayoutModule, RouterModule.forChild(routes)]
 })
 export class AppRoutingModule {}
