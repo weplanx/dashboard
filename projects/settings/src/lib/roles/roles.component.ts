9848ac1732896387dd4d8c471eecdc088333ff27
kain <zhangtqx@vip.qq.com>
2022-01-18 19:10:40 +0800

    update

--- projects/settings/src/lib/roles/roles.component.ts
+++ projects/settings/src/lib/roles/roles.component.ts
@@ -5,6 +5,7 @@ import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzModalService } from 'ng-zorro-antd/modal';
 
 import { FormComponent } from './form/form.component';
+import { PermissionComponent } from './permission/permission.component';
 import { RolesService } from './roles.service';
 import { Role } from './types';
 
@@ -34,12 +35,36 @@ export class RolesComponent implements OnInit {
     });
   }
 
+  /**
+   * 获取标签
+   */
   getLabels(): void {
     this.roles.findLabels().subscribe(data => {
       this.labels = [{ label: '全部', value: '*' }, ...data];
     });
   }
 
+  /**
+   * 权限设置表单
+   * @param editable
+   */
+  permission(editable: AnyDto<Role>): void {
+    this.modal.create({
+      nzTitle: `[${editable.name}] 权限设置`,
+      nzContent: PermissionComponent,
+      nzComponentParams: {
+        editable
+      },
+      nzOnOk: () => {
+        this.getData();
+      }
+    });
+  }
+
+  /**
+   * 编辑表单
+   * @param editable
+   */
   form(editable?: AnyDto<Role>): void {
     this.modal.create({
       nzTitle: !editable ? '新增' : '编辑',
@@ -54,6 +79,10 @@ export class RolesComponent implements OnInit {
     });
   }
 
+  /**
+   * 删除
+   * @param data
+   */
   delete(data: AnyDto<Role>): void {
     this.roles.delete(data._id).subscribe(() => {
       this.message.success('数据删除完成');
