e42a11d1c385845beab0d4eacf1db829af9e1b67
kain <zhangtqx@vip.qq.com>
2022-02-03 00:02:24 +0800

    update

--- projects/common/src/lib/directives/retry.directive.spec.ts
+++ projects/common/src/lib/directives/retry.directive.spec.ts
@@ -1,22 +1,34 @@
 import { Component, DebugElement, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { WpxModule, WpxRetryDirective, WpxShareModule } from '@weplanx/common';
-import { NzImageModule } from 'ng-zorro-antd/image';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NzImageDirective, NzImageModule } from 'ng-zorro-antd/image';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 import { fromEvent, timeout } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+import { By } from '@angular/platform-browser';
 
 @Component({
-  template: ` <img #ref nz-image wpxRetry [wpxRetryCount]="2" [wpxDelay]="500" nzSrc="/unkonw" alt="" /> `
+  template: `
+    <img
+      nz-image
+      nzSrc="/unknow.svg"
+      wpxRetry
+      [wpxRetryCount]="3"
+      [wpxDelay]="50"
+      alt=""
+    />
+  `
 })
 class ExampleComponent {
-  @ViewChild('ref') ref!: ElementRef;
+  @ViewChild(NzImageDirective) nzImage!: NzImageDirective;
   @ViewChild(WpxRetryDirective) wpxRetry!: WpxRetryDirective;
 }
 
 describe('测试 WpxRetryDirective', () => {
   let component: ExampleComponent;
   let fixture: ComponentFixture<ExampleComponent>;
+  let httpTestingController: HttpTestingController;
   let debugElement: DebugElement;
 
   beforeEach(() => {
@@ -25,6 +37,7 @@ describe('测试 WpxRetryDirective', () => {
         declarations: [ExampleComponent],
         imports: [WpxModule, WpxShareModule, NzImageModule, HttpClientTestingModule]
       });
+      httpTestingController = TestBed.inject(HttpTestingController);
       fixture = TestBed.createComponent(ExampleComponent);
       component = fixture.componentInstance;
       debugElement = fixture.debugElement;
@@ -33,10 +46,25 @@ describe('测试 WpxRetryDirective', () => {
   });
 
   it('图片资源重试', done => {
-    expect(component.wpxRetry.wpxRetryCount).toEqual(2);
-    expect(component.wpxRetry.wpxDelay).toEqual(500);
+    expect(component.wpxRetry.wpxRetryCount).toEqual(3);
+    expect(component.wpxRetry.wpxDelay).toEqual(50);
+    debugElement.query(By.directive(NzImageDirective)).triggerEventHandler('error', null);
     setTimeout(() => {
-      done();
-    }, 3000);
+      const req1 = httpTestingController.expectOne(component.nzImage.nzSrc);
+      expect(req1.request.method).toEqual('HEAD');
+      req1.flush(null, { status: 404, statusText: 'Not Found' });
+      setTimeout(() => {
+        const req2 = httpTestingController.expectOne(component.nzImage.nzSrc);
+        expect(req2.request.method).toEqual('HEAD');
+        req2.flush(null, { status: 404, statusText: 'Not Found' });
+        setTimeout(() => {
+          const req3 = httpTestingController.expectOne(component.nzImage.nzSrc);
+          expect(req3.request.method).toEqual('HEAD');
+          req3.flush(null, { status: 204, statusText: 'Not Content' });
+          httpTestingController.verify();
+          done();
+        }, 50);
+      }, 50);
+    }, 100);
   });
 });
