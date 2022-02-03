8299de81ca8361a869a76a23260e0a614278e29b
kain <zhangtqx@vip.qq.com>
2022-02-04 00:28:51 +0800

    update

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-components'),
+      dir: require('path').join(__dirname, './coverage/console'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }]
     },
--- projects/common/karma.conf.js
+++ projects/common/karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/core'),
+      dir: require('path').join(__dirname, '../../coverage/common'),
       subdir: '.',
       reporters: [
         { type: 'html', subdir: 'report-html' },
