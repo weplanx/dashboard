64f2a46ccb54ae000a28fb1a34c1ee8e43f0c63c
kain <zhangtqx@vip.qq.com>
2022-02-23 21:30:52 +0800

    更新：个人中心

--- projects/center/src/lib/profile/notify/notify.component.ts
+++ projects/center/src/lib/profile/notify/notify.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'wpx-center-profile-notify',
   templateUrl: './notify.component.html'
 })
-export class NotifyComponent {}
+export class NotifyComponent {
+  notify: any[] = [
+    { name: '通知提醒', internal: true, email: false },
+    { name: '待办提醒', internal: true, email: false },
+    { name: '订单提醒', internal: true, email: false },
+    { name: '@我', internal: true, email: false }
+  ];
+}
