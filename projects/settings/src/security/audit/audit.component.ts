1779ac1fb84127fa58a19ba8734f2643d68db546
kain <zhangtqx@vip.qq.com>
2022-04-14 19:36:01 +0800

    更新：安全

--- projects/settings/src/security/audit/audit.component.ts
+++ projects/settings/src/security/audit/audit.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'wpx-settings-security-audit',
+  selector: 'wpx-settings-audit',
   templateUrl: './audit.component.html'
 })
 export class AuditComponent {
+  searchText: string = '';
   data: any[] = [];
+
+  getData(): void {}
 }
