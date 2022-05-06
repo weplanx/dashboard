d76e5c8bd8a576b171ddb590bff0b5e68daedeb5
kain <zhangtqx@vip.qq.com>
2022-05-06 17:58:19 +0800

    更新

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,5 @@
 export const environment = {
   production: false,
   baseUrl: 'https://xapi.kainonly.com:8443',
-  cdn: 'https://cdn.kainonly.com',
-  upload: {
-    url: 'https://cdn-1251113164.cos.ap-guangzhou.myqcloud.com',
-    presignedUrl: 'uploader',
-    size: 102400,
-    storage: 'cos'
-  }
+  cdn: 'https://cdn.kainonly.com'
 };
