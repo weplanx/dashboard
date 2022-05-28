91b51ae503e2ad4d65275db2abd854d4dab8e6f8
kain <zhangtqx@vip.qq.com>
2022-05-28 15:11:30 +0800

    更新：媒体

--- projects/integrate/admin/media/pictures/pictures.component.ts
+++ projects/integrate/admin/media/pictures/pictures.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'wpx-admin-pictures',
-  templateUrl: './pictures.component.html'
+  templateUrl: './pictures.component.html',
+  styleUrls: ['./pictures.component.scss']
 })
 export class PicturesComponent {}
