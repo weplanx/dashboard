fcb8a939f1dc1d724642103550261e3ea08bdaf0
kain <zhangtqx@vip.qq.com>
2022-05-29 18:50:48 +0800

    更新：应用 > 监控（示例）

--- projects/integrate/admin/application/application.module.ts
+++ projects/integrate/admin/application/application.module.ts
@@ -7,6 +7,8 @@ import { FactoryComponent } from './factory/factory.component';
 import { FactoryModule, factory } from './factory/factory.module';
 import { FunctionsComponent } from './functions/functions.component';
 import { functions, FunctionsModule } from './functions/functions.module';
+import { MonitorComponent } from './monitor/monitor.component';
+import { MonitorModule } from './monitor/monitor.module';
 
 export const application: Routes = [
   {
@@ -25,10 +27,17 @@ export const application: Routes = [
       breadcrumb: '功能模块'
     }
   },
+  {
+    path: 'monitor',
+    component: MonitorComponent,
+    data: {
+      breadcrumb: '监控'
+    }
+  },
   { path: '', redirectTo: '/admin/application/factory/home', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [WpxShareModule, WpxModule, FactoryModule, FunctionsModule]
+  imports: [WpxShareModule, WpxModule, FactoryModule, FunctionsModule, MonitorModule]
 })
 export class ApplicationModule {}
---
+++ projects/integrate/admin/application/monitor/monitor.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { WpxModule, WpxShareModule } from '@weplanx/ng';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+
+import { HealthComponent } from './chart/health.component';
+import { LoadComponent } from './chart/load.component';
+import { ResourceComponent } from './chart/resource.component';
+import { MonitorComponent } from './monitor.component';
+
+@NgModule({
+  imports: [WpxModule, WpxShareModule],
+  declarations: [MonitorComponent, HealthComponent, LoadComponent, ResourceComponent]
+})
+export class MonitorModule {}
