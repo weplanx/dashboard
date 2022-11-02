a04b73319f3d7613a995e3f34acda4598426e27a
kain <zhangtqx@vip.qq.com>
2022-11-02 09:30:50 +0800

    UPDATE: Pages Tabs

--- src/app/pages/advanced/advanced.module.ts
+++ src/app/pages/advanced/advanced.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ShareModule } from '@common/share.module';
 
+import { TabsModule } from '../tabs/tabs.module';
 import { AdvancedComponent } from './advanced.component';
 
 const routes: Routes = [
@@ -13,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ShareModule, RouterModule.forChild(routes)],
+  imports: [ShareModule, RouterModule.forChild(routes), TabsModule],
   declarations: [AdvancedComponent]
 })
 export class AdvancedModule {}
