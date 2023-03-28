1bc8ae5ddacfa8261e844756270df0ca9449645d
kain <zhangtqx@vip.qq.com>
2023-03-28 15:54:41 +0800

    update: experiment resources

--- src/experiment/resources/pictures/pictures.component.ts
+++ src/experiment/resources/pictures/pictures.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+import { WpxService } from '@weplanx/ng';
+
 @Component({
   selector: 'app-resources-pictures',
   templateUrl: './pictures.component.html',
   styleUrls: ['./pictures.component.scss']
 })
-export class PicturesComponent {}
+export class PicturesComponent {
+  constructor(public wpx: WpxService) {}
+}
--- src/experiment/resources/videos/videos.component.ts
+++ src/experiment/resources/videos/videos.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+import { WpxService } from '@weplanx/ng';
+
 @Component({
   selector: 'app-resources-videos',
   templateUrl: './videos.component.html',
   styleUrls: ['./videos.component.scss']
 })
-export class VideosComponent {}
+export class VideosComponent {
+  constructor(public wpx: WpxService) {}
+}
