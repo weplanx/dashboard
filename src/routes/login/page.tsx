68709308166e3d0412271a75c15a30cdc87fb153
kain <zhangtqx@vip.qq.com>
2023-07-01 20:47:56 +0800

    update: login page

--- src/routes/login/page.tsx
+++ src/routes/login/page.tsx
@@ -1,61 +1,64 @@
+import * as url from 'url';
 import { CopyrightOutlined, GithubOutlined, SelectOutlined } from '@ant-design/icons';
 import { Avatar, Button, Col, Divider, Layout, Row, Space, Tabs, Typography } from 'antd';
 import { Sms } from '@/routes/login/sms';
 import { Basic } from '@/routes/login/basic';
 
-export default () => (
-  <>
-    <Row style={{ height: '100%' }}>
-      <Col span={12} style={{ background: '#fff' }}>
-        <Layout style={{ height: '100%' }}>
-          <Layout.Header style={{ background: '#fff' }}>
-            <Button size={'large'} type="text" icon={<GithubOutlined />}>
-              <b>WEPLANX</b>
-            </Button>
-          </Layout.Header>
-          <Layout.Content style={{ background: '#fff', flex: '1 1 auto' }}>
-            <div style={{ maxWidth: 360, margin: '12% auto 0' }}>
-              <Typography.Title level={2}>🚀 SIGN IN</Typography.Title>
-              <Tabs
-                size={'large'}
-                items={[
-                  {
-                    key: 'basic',
-                    label: 'EMAIL',
-                    children: <Basic />
-                  },
-                  {
-                    key: 'sms',
-                    label: 'SMS',
-                    children: <Sms />
-                  }
-                ]}
-              />
-              <Divider plain style={{ marginTop: 0 }}>
-                Or
-              </Divider>
-              <Space>
-                <a>
-                  <Avatar size={36} src={<img src={'https://cdn.kainonly.com/assets/Lark.png'} alt="lark" />} />
-                </a>
-              </Space>
-            </div>
-          </Layout.Content>
+export default () => {
+  return (
+    <>
+      <Row style={{ height: '100%' }}>
+        <Col span={12} style={{ background: '#fff' }}>
+          <Layout style={{ height: '100%' }}>
+            <Layout.Header style={{ background: '#fff' }}>
+              <Button size={'large'} type="text" icon={<GithubOutlined />}>
+                <b>STAR</b>
+              </Button>
+            </Layout.Header>
+            <Layout.Content style={{ background: '#fff', flex: '1 1 auto' }}>
+              <div style={{ maxWidth: 360, margin: '9% auto 0' }}>
+                <Typography.Title level={2}>SIGN IN</Typography.Title>
+                <Tabs
+                  size={'large'}
+                  items={[
+                    {
+                      key: 'basic',
+                      label: 'EMAIL',
+                      children: <Basic />
+                    },
+                    {
+                      key: 'sms',
+                      label: 'SMS',
+                      children: <Sms />
+                    }
+                  ]}
+                />
+                <Divider plain style={{ marginTop: 0 }}>
+                  Or
+                </Divider>
+                <Space>
+                  <a>
+                    <Avatar size={36} src={<img src={'https://cdn.kainonly.com/assets/Lark.png'} alt="lark" />} />
+                  </a>
+                </Space>
+              </div>
+            </Layout.Content>
 
-          <Layout.Footer style={{ background: '#fff', textAlign: 'center' }}>
-            <Space direction={'vertical'}>
-              <Typography.Link style={{ color: 'rgba(0, 0, 0, 0.85)' }} strong>
-                <span>BSD-3-Clause License </span>
-                <SelectOutlined />
-              </Typography.Link>
-              <Typography.Text strong>
-                Copyright <CopyrightOutlined /> 2023. Open Source DevOps by WEPLANX.
-              </Typography.Text>
-            </Space>
-          </Layout.Footer>
-        </Layout>
-      </Col>
-      <Col span={12} />
-    </Row>
-  </>
-);
+            <Layout.Footer style={{ background: '#fff', textAlign: 'center' }}>
+              <Space direction={'vertical'}>
+                <Typography.Link style={{ color: 'rgba(0, 0, 0, 0.85)' }} strong>
+                  <span>BSD-3-Clause License </span>
+                  <SelectOutlined />
+                </Typography.Link>
+                <Typography.Text strong>
+                  Copyright <CopyrightOutlined /> 2023. Open Source DevOps by WEPLANX.
+                </Typography.Text>
+              </Space>
+            </Layout.Footer>
+          </Layout>
+        </Col>
+        <Col span={12} />
+      </Row>
+    </>
+  );
+};
