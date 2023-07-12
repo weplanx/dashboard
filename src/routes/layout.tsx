d11bca508c95bf740f220ce8890410f5d3635bcf
kain <zhangtqx@vip.qq.com>
2023-07-12 11:02:08 +0800

    update: prettier

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from '@modern-js/runtime/router';
-import { Layout } from 'antd';
-import '@/common/styles/base.scss';
+import { Layout, ConfigProvider } from 'antd';
+import '@common/styles/base.scss';
 
 export default () => {
   return (
-    <Layout style={{ height: '100%', overflow: 'hidden' }}>
-      <Outlet />
-    </Layout>
+    <ConfigProvider>
+      <Layout style={{ height: '100%', overflow: 'hidden' }}>
+        <Outlet />
+      </Layout>
+    </ConfigProvider>
   );
 };
